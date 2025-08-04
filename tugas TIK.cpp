d949f1761ca36f3661352b314976ae03771fe8d4
khoerunisap27 <khoerunisap27@gmail.com>
2025-08-04 12:57:24 +0700

    Add files via upload

---
+++ tugas TIK.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << " khoerunisa putri kamelia !" << endl;
+    cout << "x ips " << endl;
+    cout << "MAs Almanshuriyah"<< endl;
+    cout << "15 tahun" << endl;
+    cout << "cianjur,10 februari 2010" << endl;
+    return 0;
+}
